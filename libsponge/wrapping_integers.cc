b4503e0890812ff1cce16f3577b1f78f6cf9bfc3
MisakaXiang <975737669@qq.com>
2021-09-12 10:50:54 +0800

    chore:3.1 Translating between 64-bit indexes and 32-bit seqnos

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -14,8 +14,8 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    uint32_t tmp = (n << 32) >> 32; // 截取64位索引（从0开始）的后32位，并加上带初始序号的WrappingInt32对象即可
+    return isn + tmp;
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +29,16 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    uint64_t absolute_seqno_64 = n.raw_value() - isn.raw_value();  // 减去传输时随机生成的isn
+    if (checkpoint <= absolute_seqno_64)    // 比较少见的情况
+        return absolute_seqno_64;
+    else {  // 还原成64位,找到离checkpoint最近的那个数,且大于checkpoint
+        uint64_t size_period = 1ul << 32, quotient, remainder;   
+        quotient = (checkpoint - absolute_seqno_64) >> 32;    // 商
+        remainder = ((checkpoint - absolute_seqno_64) << 32) >> 32;    // 余数
+        if (remainder < size_period / 2)
+            return absolute_seqno_64 + quotient * size_period;
+        else
+            return absolute_seqno_64 + (quotient + 1) * size_period;
+    }
 }
