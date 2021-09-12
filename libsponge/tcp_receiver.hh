0b3163331d3f07d3dd4bf91e4df4333563472a5c
MisakaXiang <975737669@qq.com>
2021-09-12 10:51:17 +0800

    chore: Implementing the TCP receiver

--- libsponge/tcp_receiver.hh
+++ libsponge/tcp_receiver.hh
@@ -19,6 +19,8 @@ class TCPReceiver {
 
     //! The maximum number of bytes we'll store.
     size_t _capacity;
+    WrappingInt32 _isn;
+    bool _isn_set;
 
   public:
     //! \brief Construct a TCP receiver
